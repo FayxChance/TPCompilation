2b778524fc4dcb0eca930d541c24c5ef258178cf
FayxChance <fayxchancetom@outlook.com>
2020-11-30 23:52:42 +0100

    Add constructor

--- src/main/java/fr/usmb/m1isc/compilation/tp/Node.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/Node.java
@@ -1,15 +1,25 @@
 package fr.usmb.m1isc.compilation.tp;
 
 public class Node {
-    enum NodeType{ENTIER, STRING, SIGNE }
     public String _v;
     public Node _left,_right;
-    public NodeType _type;
 
     public Node(String v){
             _v=v;
             _left=null;
             _right=null;
+
+    }
+    public Node(String v,Node e1, Node e2){
+        _v=v;
+        _left=e1;
+        _right=e2;
+
+    }
+    public Node(String v,Object e1, Object e2){
+        _v=v;
+        _left=(Node)e1;
+        _right=(Node)e2;
 
     }
 }
