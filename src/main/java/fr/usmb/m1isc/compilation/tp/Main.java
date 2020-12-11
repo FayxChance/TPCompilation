5c87eda4881777525e32bf25c49c684e87f3949a
FayxChance <fayxchancetom@outlook.com>
2020-12-11 11:36:16 +0100

    Ecriture fichier segment data et debut generer

--- src/main/java/fr/usmb/m1isc/compilation/tp/Main.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/Main.java
@@ -1,21 +1,39 @@
 package fr.usmb.m1isc.compilation.tp;
 
-import java.io.FileReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Set;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception  {
-		System.out.println("COMMENCER SAISIE");
+	public static void main(String[] args) throws Exception {
 		LexicalAnalyzer yy;
-		 if (args.length > 0)
-		        yy = new LexicalAnalyzer(new FileReader(args[0])) ;
-		    else
-		        yy = new LexicalAnalyzer(new InputStreamReader(System.in)) ;
+		if (args.length > 0)
+			yy = new LexicalAnalyzer(new FileReader(args[0]));
+		else
+			yy = new LexicalAnalyzer(new InputStreamReader(System.in));
 		@SuppressWarnings("deprecation")
-		parser p = new parser (yy);
+		parser p = new parser(yy);
 
-		System.out.println(p.parse( ).value);
+		//System.out.println(p.parse( ).value+"\n");
+
+		Node racine = (Node) p.parse().value;
+		System.out.println(racine.toString());
+
+		// Segment Data
+		Set<String> data;
+		data = racine.getLet();
+		String dataWrite = "DATA SEGMENT \n";
+		for (String i : data)
+			dataWrite += "\t" + i + "\n";
+		dataWrite += "DATA ENDS \n";
+
+
+		try (FileWriter fw = new FileWriter("prog.txt", false);
+			 BufferedWriter bw = new BufferedWriter(fw);
+			 PrintWriter out = new PrintWriter(bw)) {
+			out.print(dataWrite);
+		} catch (IOException e) {
+		}
 	}
 
 }
--- src/main/java/fr/usmb/m1isc/compilation/tp/Node.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/Node.java
@@ -1,23 +1,28 @@
 package fr.usmb.m1isc.compilation.tp;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Node {
-    public enum NodeType{SEQUENCE,EXPRESSION,EXPR, VAR, INT, OUTPUT, INPUT, NIL}
+    public enum NodeType {SEQUENCE, EXPRESSION, EXPR, VAR, INT, OUTPUT, INPUT, NIL}
+
     public NodeType _t;
     public String _v;
-    public Node _left,_right;
+    public Node _left, _right;
 
-    public Node(NodeType t,String v){
-            _t=t;
-            _v=v;
-            _left=null;
-            _right=null;
+    public Node(NodeType t, String v) {
+        _t = t;
+        _v = v;
+        _left = null;
+        _right = null;
 
     }
-    public Node(NodeType t,String v,Node e1, Node e2){
-        _t=t;
-        _v=v;
-        _left=e1;
-        _right=e2;
+
+    public Node(NodeType t, String v, Node e1, Node e2) {
+        _t = t;
+        _v = v;
+        _left = e1;
+        _right = e2;
 
 
     }
@@ -27,17 +32,61 @@ public class Node {
         if (!(_left==null && _right==null))
             str = "(";
 
-        str=str.concat(_v);
+        str = str.concat(_v);
 
         if (_left != null) {
-            str=str.concat(" "+_left.toString()+" ");
+            str = str.concat(" " + _left.toString() + " ");
         }
         if (_right != null) {
-            str=str.concat(" "+_right.toString()+" ");
+            str = str.concat(" " + _right.toString() + " ");
         }
-        if (!(_left==null && _right==null))
-            str=str.concat(")");
-        
+        if (!(_left == null && _right == null))
+            str = str.concat(")");
+
         return str;
+    }
+
+    public Set<String> getLet() {
+
+        Set<String> s = new HashSet<String>();
+        if ((this._t) == NodeType.EXPRESSION) {
+            s.add(this._left._v);
+        }
+        Set<String> setLeft = this._left != null ? this._left.getLet() : null;
+        Set<String> setRight = this._right != null ? this._right.getLet() : null;
+        if (setLeft != null)
+            s.addAll(setLeft);
+        if (setRight != null)
+            s.addAll(setRight);
+        return s;
+    }
+
+    public String generer() {
+        switch (this._t) {
+            case SEQUENCE:
+                return genererExpr();
+            case EXPRESSION:
+                return genererExpr();
+            case EXPR:
+                return genererExpr();
+            case VAR:
+                return genererExpr();
+            case INT:
+                return genererExpr();
+            case OUTPUT:
+                return genererExpr();
+            case INPUT:
+                return genererExpr();
+            case NIL:
+                return genererExpr();
+            default:
+                return genererExpr();
+        }
+
+    }
+
+    public String genererExpr() {
+
+        return "";
     }
 }
